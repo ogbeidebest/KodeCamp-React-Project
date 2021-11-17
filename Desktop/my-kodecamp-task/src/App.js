d6df69b18ddc7f994a51e57688591e507d601f50
ogbeidebest <ogbeidebest.ob@gmail.com>
2021-11-17 07:53:01 +0100

    Create a React app

---
+++ Desktop/my-kodecamp-task/src/App.js
@@ -0,0 +1,45 @@
+import Navbar from './Components/Navbar';
+import Navs from './Components/Navs';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import PlanetList from './Components/PlanetList';
+import { Switch } from 'react-router';
+import PlanetApp from './Components/PlanetApp';
+import Charater from './Components/Charater';
+import CharaterList from './Components/CharaterList';
+
+
+
+
+
+function App() {
+
+    return (<Router>
+        <div className="App" >
+
+            <Navbar />
+            <Navs />
+            <div className="content">
+                <Switch>
+                    <Route exact path="/">
+                        <PlanetList />
+                    </Route >
+                    <Route exact path="/CharaterList">
+                        <CharaterList />
+                    </Route >
+
+                    <Route exact path="/PlanetApp">
+                        <PlanetApp />
+                    </Route >
+                    <Route exact path="/Charater">
+                        <Charater />
+                    </Route >
+                </Switch>
+            </div>
+        </div>
+
+    </Router >
+
+    )
+}
+
+export default App;
